961bd8e015a991e5adca9b0fe8d8fff401337bff
AlmasKanwal <almaskanwal20ak@gmail.com>
2025-09-08 21:02:51 +0500

    Alibaba.com

---
+++ Assignment-06/index.js
@@ -0,0 +1,86 @@
+let currentIndex = 0;
+const track = document.getElementById('sliderTrack');
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function moveSlide(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    track.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+// Optional autoplay
+// setInterval(() => {
+//     moveSlide(1);
+// }, 4000);
+
+
+
+// <!-- Nav Fixed Start -->
+
+const navbar = document.getElementById('mainNavbar');
+const navLogo = document.getElementById('navLogo');
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+        navbar.classList.add('fixed-nav');
+        navLogo.src = "./assets/orange-logo.png";
+    } else {
+        navbar.classList.remove('fixed-nav');
+        navLogo.src = "./assets/white-logo.png";
+    }
+});
+
+//   <!-- Nav Fixed End -->
+
+//   <!-- Search Flow Image Start -->
+
+const steps = document.querySelectorAll(".step");
+const previewImage = document.getElementById("preview-image");
+
+steps.forEach(step => {
+    ["mouseenter", "click"].forEach(evt => {
+        step.addEventListener(evt, () => {
+            // Remove old active
+            steps.forEach(s => s.classList.remove("active"));
+
+            // Add active on current
+            step.classList.add("active");
+
+            // Image switch with fade
+            const imgSrc = step.getAttribute("data-img");
+            previewImage.style.opacity = 0;
+            setTimeout(() => {
+                previewImage.src = imgSrc;
+                previewImage.style.opacity = 1;
+            }, 200);
+        });
+    });
+});
+//   <!-- Search Flow Image End -->
+
+
+let slideIndex = 0;
+        var slider = document.getElementsByClassName("slide");
+
+        function showSlides(n) {
+            if (n >= slider.length) { slideIndex = 0 }
+            if (n < 0) { slideIndex = slider.length - 1 }
+            for (let i = 0; i < slider.length; i++) {
+                slider[i].classList.remove("active");
+            }
+            slider[slideIndex].classList.add("active");
+        }
+
+        function plusSlides(n) {
+            slideIndex += n;
+            showSlides(slideIndex);
+        }
+
+        showSlides(slideIndex);
